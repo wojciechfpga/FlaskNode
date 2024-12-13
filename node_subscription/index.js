df9634336114b6913194b32700080e12d8396029
wojciechfpga <lasowski.wojciech.poczta@gmail.com>
2024-12-14 00:18:08 +0100

    DB trigger

--- node_subscription/index.js
+++
@@ -1,9 +0,0 @@
-const { ApolloServer } = require('apollo-server');
-const typeDefs = require('./src/graphql/typeDefs');
-const resolvers = require('./src/graphql/resolvers');
-
-const server = new ApolloServer({ typeDefs, resolvers });
-
-server.listen(4000).then(({ url }) => {
-  console.log(`🚀 Subscriptions ready at ${url}`);
-});
